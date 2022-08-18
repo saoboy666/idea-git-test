2ddfbf26e481f0c3d66256a96fa676ff0071201a
zuoguaizi <1920946636@qq.com>
2022-08-18 18:24:56 +0800

    新增一个输出语句

--- src/com/liuao/demos/Test1.java
+++ src/com/liuao/demos/Test1.java
@@ -3,5 +3,6 @@ package com.liuao.demos;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("GitHub添加的内容");
+        System.out.println("新增输出语句1");
     }
 }
